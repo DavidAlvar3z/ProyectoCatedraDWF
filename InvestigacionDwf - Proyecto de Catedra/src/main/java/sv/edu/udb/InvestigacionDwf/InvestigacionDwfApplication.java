85c8338bb629b852a1249cae53d639a114d071da
DavidAlvar3z <AM240104@alumno.udb.edu.sv>
2025-10-16 20:17:45 -0600

    clase principal de Spring Boot
    - @SpringBootApplication
    - Punto de entrada de la aplicación
    - Main method para ejecutar el proyecto

--- InvestigacionDwf - Proyecto de Catedra/src/main/java/sv/edu/udb/InvestigacionDwf/InvestigacionDwfApplication.java
+++ InvestigacionDwf - Proyecto de Catedra/src/main/java/sv/edu/udb/InvestigacionDwf/InvestigacionDwfApplication.java
@@ -2,12 +2,12 @@ package sv.edu.udb.InvestigacionDwf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
 public class InvestigacionDwfApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(InvestigacionDwfApplication.class, args);
     }
-
 }

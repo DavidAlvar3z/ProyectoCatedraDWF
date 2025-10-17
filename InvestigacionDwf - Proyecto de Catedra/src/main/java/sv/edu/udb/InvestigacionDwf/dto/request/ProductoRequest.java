2b1afd5652efa2d7e9bdf988f068c24d82aaee87
achi-vonz <VG240979@alumno.udb.edu.sv>
2025-10-16 20:59:35 -0600

    DTOs de productos y categorías
    ProductoRequest:
    - Validaciones: @NotNull para precio, costo, cantidad
    - @Min(0) para valores numéricos
    - Campo idTipoProducto para categorización
    
    TipoProductoRequest:
    - Tipo y descripción con @NotBlank
    
    ProductoResponse:
    - Incluye información completa del producto
    - TipoProducto embebido
    - Compatible con HATEOAS
    
    TipoProductoResponse:
    - DTO de salida simple con builder

---
+++ InvestigacionDwf - Proyecto de Catedra/src/main/java/sv/edu/udb/InvestigacionDwf/dto/request/ProductoRequest.java
@@ -0,0 +1,41 @@
+package sv.edu.udb.InvestigacionDwf.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ProductoRequest {
+
+    @NotNull(message = "El nombre es requerido")
+    private String nombre;
+
+    @NotNull(message = "La descripción es requerida")
+    private String descripcion;
+
+    @NotNull(message = "El precio es requerido")
+    private BigDecimal precio;
+
+    @NotNull(message = "El costo es requerido")
+    private BigDecimal costo;
+
+    @NotNull(message = "La cantidad es requerida")
+    private Integer cantidad;
+
+    private String imagen;
+
+    @NotNull(message = "La cantidad de puntos es requerida")
+    private Integer cantidadPuntos;
+
+    @NotNull(message = "El ID del tipo de producto es requerido")
+    private Long idTipoProducto;
+}
---
+++ InvestigacionDwf - Proyecto de Catedra/src/main/java/sv/edu/udb/InvestigacionDwf/dto/request/TipoProductoResponse.java
@@ -0,0 +1,16 @@
+package sv.edu.udb.InvestigacionDwf.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TipoProductoResponse {
+    private Long idTipoProducto;
+    private String tipo;
+    private String descripcion;
+}

030597452860f59543f255bd30aa15b52acd3c1e
achi-vonz <VG240979@alumno.udb.edu.sv>
2025-10-16 21:10:39 -0600

    assemblers HATEOAS para hypermedia
    Todos los assemblers implementan RepresentationModelAssembler:
    
    ProductoAssembler:
    - Links: self, collection, tipo-producto
    - Integración con ProductoController
    
    PedidoAssembler:
    - Links: self, collection, user, items
    - Estados y transiciones
    
    ResenaAssembler:
    - Links: self, producto, user
    - Integración con ResenaController
    
    UserAssembler:
    - Links: self, collection, pedidos
    - Sin información sensible
    
    HistorialPedidoAssembler y HistorialPuntosAssembler:
    - Links para navegación de historial
    - Paginación con links (first, last, next, prev)
    - Integración con controllers

---
+++ InvestigacionDwf - Proyecto de Catedra/src/main/java/sv/edu/udb/InvestigacionDwf/service/assembler/PedidoAssembler.java
@@ -0,0 +1,40 @@
+package sv.edu.udb.InvestigacionDwf.service.assembler;
+
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import sv.edu.udb.InvestigacionDwf.controller.PedidoController;
+import sv.edu.udb.InvestigacionDwf.dto.response.PedidoResponse;
+import sv.edu.udb.InvestigacionDwf.model.entity.Pedido;
+import sv.edu.udb.InvestigacionDwf.service.mapper.PedidoMapper;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class PedidoAssembler implements RepresentationModelAssembler<Pedido, PedidoResponse> {
+
+    private final PedidoMapper pedidoMapper;
+
+    public PedidoAssembler(PedidoMapper pedidoMapper) {
+        this.pedidoMapper = pedidoMapper;
+    }
+
+    @Override
+    public PedidoResponse toModel(Pedido pedido) {
+        PedidoResponse response = pedidoMapper.toResponse(pedido);
+
+        // self link (usa el GET /auth/pedido/{id})
+        response.add(linkTo(methodOn(PedidoController.class)
+                .getById(pedido.getIdPedido()))
+                .withSelfRel());
+
+        // enlace a la lista de pedidos del usuario
+        // Como Pageable y PagedResourcesAssembler no pueden ser null, usamos una URI manual o omitimos este link
+        response.add(linkTo(PedidoController.class)
+                .slash("user")
+                .slash(pedido.getCarrito().getUser().getIdUser())
+                .withRel("pedidos-usuario"));
+
+        return response;
+    }
+}
---
+++ InvestigacionDwf - Proyecto de Catedra/src/main/java/sv/edu/udb/InvestigacionDwf/service/assembler/ProductoAssembler.java
@@ -0,0 +1,40 @@
+// src/main/java/sv/edu/udb/InvestigacionDwf/service/assembler/ProductoAssembler.java
+package sv.edu.udb.InvestigacionDwf.service.assembler;
+
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import sv.edu.udb.InvestigacionDwf.controller.ProductoController;
+import sv.edu.udb.InvestigacionDwf.dto.response.ProductoResponse;
+import sv.edu.udb.InvestigacionDwf.model.entity.Producto;
+import sv.edu.udb.InvestigacionDwf.service.mapper.ProductoMapper;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class ProductoAssembler implements RepresentationModelAssembler<Producto, ProductoResponse> {
+
+    private final ProductoMapper mapper;
+
+    public ProductoAssembler(ProductoMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public ProductoResponse toModel(Producto producto) {
+        ProductoResponse response = mapper.toResponse(producto);
+
+        // self link
+        response.add(linkTo(methodOn(ProductoController.class)
+                .getById(producto.getIdProducto()))
+                .withSelfRel());
+
+        // enlace a la lista paginada de productos
+        response.add(linkTo(ProductoController.class)
+                .slash("all")
+                .withRel("productos"));
+
+        return response;
+    }
+}
+
